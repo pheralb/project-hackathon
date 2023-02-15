89624f7a5f3daa98f16ffcefe885c414581875fa
pheralb <pablohdez4340@gmail.com>
2023-02-15 09:24:23 +0000

    🛠️ Create link component.

---
+++ src/ui/link.tsx
@@ -0,0 +1,34 @@
+import clsx from "clsx";
+import NextLink from "next/link";
+import type { ReactNode } from "react";
+
+interface LinkProps {
+  href: string;
+  children: ReactNode;
+  className?: string;
+  underline?: boolean;
+}
+
+export const ExternalLink = (props: LinkProps) => {
+  return (
+    <a
+      href={props.href}
+      rel="noreferer noopener"
+      target="_blank"
+      className={clsx(props.underline && "underline", props.className)}
+    >
+      {props.children}
+    </a>
+  );
+};
+
+export const Link = (props: LinkProps) => {
+  return (
+    <NextLink
+      href={props.href}
+      className={clsx(props.underline && "underline", props.className)}
+    >
+      {props.children}
+    </NextLink>
+  );
+};
