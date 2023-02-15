ac90c7f22bf93c4c767e7ea37f64e5be974bee16
pheralb <pablohdez4340@gmail.com>
2023-02-15 09:24:39 +0000

    🛠️ First home design.

---
+++ src/layout/footer.tsx
@@ -0,0 +1,25 @@
+import { FrameSimple } from "iconoir-react";
+import { ExternalLink } from "@/ui/link";
+
+const Footer = () => {
+  return (
+    <div className="fixed bottom-0 w-full py-3 px-5 font-medium text-gray-400">
+      <div className="flex flex-row items-center justify-between">
+        <p>v0.1.0</p>
+        <div className="flex items-center space-x-1">
+          <FrameSimple width={16} className="mr-1" />
+          <p>Crafted by</p>
+          <ExternalLink href="https://twitter.com/pheralb_" underline={true}>
+            pheralb
+          </ExternalLink>
+          <p>&</p>
+          <ExternalLink href="https://twitter.com/tmchein" underline={true}>
+            tmchein
+          </ExternalLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,55 @@
+import { motion } from "framer-motion";
+import { ArrowRight } from "iconoir-react";
+import Footer from "@/layout/footer";
+import { Link } from "@/ui/link";
+
 export default function Home() {
   return (
     <>
-      <h1 className="">Hello World</h1>
+      <div className="flex h-screen flex-col items-center justify-center space-y-4">
+        <motion.div
+          initial={{ y: -7, opacity: 0 }}
+          animate={{ y: 0, opacity: 0.9 }}
+          transition={{
+            duration: 0.5,
+            delay: 0.3,
+          }}
+          className="px-4 text-center md:px-0"
+        >
+          <h1 className="text-2xl md:text-3xl">
+            An open source hackathon management
+          </h1>
+        </motion.div>
+        <Link
+          href="app"
+          underline={false}
+          className="transition-all duration-200 hover:translate-x-0.5"
+        >
+          <motion.div
+            initial={{ y: 0.3, opacity: 0 }}
+            animate={{ y: 0, opacity: 0.9 }}
+            transition={{
+              duration: 0.4,
+              delay: 1,
+            }}
+          >
+            <div className="flex items-center space-x-2">
+              <span>Get Started</span>
+              <ArrowRight width={16} />
+            </div>
+          </motion.div>
+        </Link>
+      </div>
+      <motion.div
+        initial={{ y: 0.3, opacity: 0 }}
+        animate={{ y: 0, opacity: 0.9 }}
+        transition={{
+          duration: 0.4,
+          delay: 0.5,
+        }}
+      >
+        <Footer />
+      </motion.div>
     </>
-  )
+  );
 }
