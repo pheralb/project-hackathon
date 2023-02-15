ea39c837ffd8372349a0cb82049d1f6b83985342
pheralb <pablohdez4340@gmail.com>
2023-02-15 15:06:41 +0000

    🛠️ Modify animation properties.

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,10 +1,19 @@
+import { motion } from "framer-motion";
 import { FrameSimple } from "iconoir-react";
 import { ExternalLink } from "@/ui/link";
 
 const Footer = () => {
   return (
     <div className="fixed bottom-0 w-full py-3 px-5 font-medium text-gray-400">
-      <div className="flex flex-row items-center justify-between">
+      <motion.div
+        className="flex flex-row items-center justify-between"
+        initial={{ y: 0.3, opacity: 0 }}
+        animate={{ y: 0, opacity: 0.9 }}
+        transition={{
+          duration: 0.4,
+          delay: 0.5,
+        }}
+      >
         <p>v0.1.0</p>
         <div className="flex items-center space-x-1">
           <FrameSimple width={16} className="mr-1" />
@@ -17,7 +26,7 @@ const Footer = () => {
             tmchein
           </ExternalLink>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
