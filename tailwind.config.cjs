25f34394da56a56a896a7c345d094928a6acd99d
pheralb <pablohdez4340@gmail.com>
2023-02-16 23:43:23 +0000

    🛠️ Update tailwind.config.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,10 +4,24 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--dmsans-font)", "sans-serif"],
+        sans: ["Satoshi", "sans-serif"],
       },
       colors: {
         midnight: "#161616",
+      },
+      keyframes: {
+        overlayShow: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        contentShow: {
+          from: { opacity: 0, transform: "translate(-50%, -48%) scale(0.96)" },
+          to: { opacity: 1, transform: "translate(-50%, -50%) scale(1)" },
+        },
+      },
+      animation: {
+        overlayShow: "overlayShow 300ms cubic-bezier(0.16, 1, 0.3, 1)",
+        contentShow: "contentShow 300ms cubic-bezier(0.16, 1, 0.3, 1)",
       },
     },
   },
