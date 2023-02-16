bf71c88bfdee82785c3c9634080f76e9fb5b46a6
pheralb <pablohdez4340@gmail.com>
2023-02-16 20:32:29 +0000

    🛠️ Update document.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className='bg-midnight text-white'>
+      <body className="bg-midnight font-sans text-white">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
