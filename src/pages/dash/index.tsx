f32695e8fa45379adfbc24e8cc95a54b687402e3
pheralb <pablohdez4340@gmail.com>
2023-02-20 09:49:47 +0000

    🛠️ Design improvements.

--- src/pages/dash/index.tsx
+++ src/pages/dash/index.tsx
@@ -7,7 +7,7 @@ const Dashboard = () => {
     <div className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="mb-2 text-3xl font-medium">Welcome</h1>
       <p>What do you want to do?</p>
-      <div className="mt-6 max-w-md">
+      <div className="mt-6 max-w-md border-b border-neutral-800 pb-3">
         <div className="mb-2 flex w-full items-center justify-between space-x-2">
           <CreateNew />
           <EnterKey />
@@ -17,6 +17,7 @@ const Dashboard = () => {
           <strong>Enter hackathon key</strong>.
         </Tip>
       </div>
+      sdfsd
     </div>
   );
 };
