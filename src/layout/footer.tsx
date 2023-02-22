83ac444bbc178b960e345db7ff635c2a798025c3
pheralb <pablohdez4340@gmail.com>
2023-02-22 09:54:44 +0000

    🛠️ Update footer.

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -4,17 +4,19 @@ import { ExternalLink } from "@/ui";
 const Footer = () => {
   return (
     <div className="fixed bottom-0 w-full py-3 px-5 font-medium text-gray-400">
-      <p>v0.1.0</p>
-      <div className="flex items-center space-x-1">
-        <FrameSimple width={16} className="mr-1" />
-        <p>Crafted by</p>
-        <ExternalLink href="https://twitter.com/pheralb_" underline={true}>
-          pheralb
-        </ExternalLink>
-        <p>&</p>
-        <ExternalLink href="https://twitter.com/tmchein" underline={true}>
-          tmchein
-        </ExternalLink>
+      <div className="flex items-center justify-between">
+        <p>v0.1.0</p>
+        <div className="flex items-center space-x-1">
+          <FrameSimple width={16} className="mr-1" />
+          <p>Crafted by</p>
+          <ExternalLink href="https://twitter.com/pheralb_" underline={true}>
+            pheralb
+          </ExternalLink>
+          <p>&</p>
+          <ExternalLink href="https://twitter.com/tmchein" underline={true}>
+            tmchein
+          </ExternalLink>
+        </div>
       </div>
     </div>
   );
