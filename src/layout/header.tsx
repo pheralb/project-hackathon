3bf6073d2111d7242ded4335263d9ba9dc4c4b24
pheralb <pablohdez4340@gmail.com>
2023-02-22 08:32:21 +0000

    🛠️ Fix header style.

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -18,25 +18,27 @@ const Header = () => {
 
   return (
     <div className="fixed top-0 w-full py-3 px-5 font-medium text-gray-200">
-      <Link href="/" underline={false}>
-        <div className="flex items-center space-x-3 transition-all duration-100 hover:text-white">
-          <Image
-            src="/images/phck.svg"
-            width={40}
-            height={40}
-            alt="Project Hackathon Logo"
-          />
-          <p>Project Hackathon</p>
-        </div>
-      </Link>
-      {session && (
-        <div className="flex items-center space-x-3">
-          <img src={session.user.image} className="h-6 w-6 rounded-full" />
-          <p>{session.user.name}</p>
-          <span className="text-gray-400">|</span>
-          <Button onClick={handleLogout}>Sign out</Button>
-        </div>
-      )}
+      <div className="flex items-center justify-between">
+        <Link href="/" underline={false}>
+          <div className="flex items-center space-x-3 transition-all duration-100 hover:text-white">
+            <Image
+              src="/images/phck.svg"
+              width={40}
+              height={40}
+              alt="Project Hackathon Logo"
+            />
+            <p>Project Hackathon</p>
+          </div>
+        </Link>
+        {session && (
+          <div className="flex items-center space-x-3">
+            <img src={session.user.image} className="h-6 w-6 rounded-full" />
+            <p>{session.user.name}</p>
+            <span className="text-gray-400">|</span>
+            <Button onClick={handleLogout}>Sign out</Button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
