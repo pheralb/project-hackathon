b9e29a128a36ede3b5347be60072c573a9a502e6
pheralb <pablohdez4340@gmail.com>
2023-02-23 17:25:16 +0000

    🛠️ Update middleware.

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,7 @@ export default withAuth(
 
     if (isAuthPage) {
       if (isAuth) {
-        return NextResponse.redirect(new URL("/dash", req.url));
+        return NextResponse.redirect(new URL("/app", req.url));
       }
 
       return null;
@@ -37,5 +37,5 @@ export default withAuth(
 );
 
 export const config = {
-  matcher: ["/dash/:path*", "/auth"],
+  matcher: ["/app/:path*", "/auth"],
 };
