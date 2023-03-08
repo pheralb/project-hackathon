6d45952db52cacc713d3bf03fbd8777bb6e35042
pheralb <pablohdez4340@gmail.com>
2023-03-08 14:08:33 +0000

    🛠️ Check if the user has already participated in the hackathon.

--- src/pages/send/[url].tsx
+++ src/pages/send/[url].tsx
@@ -4,16 +4,14 @@ import { useRouter } from "next/router";
 
 import Loading from "@/components/loading";
 import SendProject from "@/components/sendProject";
+import { Link } from "@/ui";
+import { ButtonStyles } from "@/ui/button";
 
 const Send = () => {
   const router = useRouter();
   const { url } = router.query;
 
-  const {
-    data: hackathon,
-    isLoading,
-    error,
-  } = api.hackathon.singleHackathon.useQuery({
+  const { data, isLoading, error } = api.hackathon.singleHackathon.useQuery({
     url: url as string,
   });
 
@@ -25,21 +23,35 @@ const Send = () => {
     );
   }
 
-  if (error || !hackathon) {
+  if (error || !data.hackathon) {
     return router.push("/404");
   }
 
   return (
     <>
       <Head>
-        <title>Send project: {hackathon.name} - Project Hackathon</title>
+        <title>Send project: {data.hackathon.name} - Project Hackathon</title>
       </Head>
       <div className="mx-auto flex h-screen flex-col items-center justify-center">
-        <SendProject
-          id={hackathon.id}
-          url={hackathon.url}
-          name={hackathon.name}
-        />
+        {data.participants.length === 0 ? (
+          <SendProject
+            id={data.hackathon.id}
+            url={data.hackathon.url}
+            name={data.hackathon.name}
+          />
+        ) : (
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="mb-3 text-center text-3xl font-medium">
+              You already sent a project for this hackathon
+            </h1>
+            <p className="mb-3 text-center text-gray-400">
+              You can only send one project per hackathon
+            </p>
+            <Link href="/app" className={ButtonStyles}>
+              Go to dashboard
+            </Link>
+          </div>
+        )}
       </div>
     </>
   );
