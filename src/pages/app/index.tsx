90576a5819e9ffc80fc4f16851cdd13341f14195
pheralb <pablohdez4340@gmail.com>
2023-03-08 01:26:00 +0000

    🛠️ Dashboard improvements.

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,28 +1,33 @@
+import { useState } from "react";
 import Head from "next/head";
 import { api } from "@/trpc/api";
 import type { Hackathon } from "@prisma/client";
-import { useState } from "react";
 
-import CreateNew from "@/components/createNew";
-import EnterKey from "@/components/enterKey";
-import HackathonCard from "@/components/hackathonCard";
-import Loading from "@/components/loading";
 import { Input, Tip } from "@/ui";
 import { ArrowDown } from "@/ui/icons";
 import Up from "@/animations/up";
 import Down from "@/animations/down";
 
+import CreateNew from "@/components/createNew";
+import EnterKey from "@/components/enterKey";
+import HackathonCard from "@/components/hackathonCard";
+import Loading from "@/components/loading";
+
 const Dashboard = () => {
   const [search, setSearch] = useState("");
-  const {
-    data: hackathons,
-    isLoading,
-    error,
-  } = api.hackathon.allHackathons.useQuery();
+  const { data, isLoading, error } = api.hackathon.allHackathons.useQuery();
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
+
+  if (isLoading) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <Loading />
+      </div>
+    );
+  }
 
   return (
     <>
@@ -37,49 +42,79 @@ const Dashboard = () => {
         </div>
       </div>
       <div className="mx-auto mt-8 max-w-6xl px-6 md:px-0">
-        {isLoading ? (
-          <div className="mt-6">
-            <Loading />
-          </div>
-        ) : hackathons && hackathons?.length > 0 ? (
-          <>
-            <Input
-              value={search}
-              placeholder="Search..."
-              onChange={handleSearch}
-            />
-            <div className="container mx-auto mt-4">
-              <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-                {hackathons.map(
-                  (hackathon: Hackathon) =>
-                    hackathon.name.includes(search) && (
-                      <HackathonCard
-                        key={hackathon.id}
-                        name={hackathon.name}
-                        description={hackathon.description || ""}
-                        url={hackathon.url}
-                      />
-                    ),
-                )}
+        <div className="border-b border-neutral-800 pb-6">
+          <h1 className="mb-4 text-2xl font-medium">Hackathons</h1>
+          {data?.hackathon && data?.hackathon?.length > 0 ? (
+            <>
+              <Input
+                value={search}
+                placeholder="Search..."
+                onChange={handleSearch}
+              />
+              <div className="container mx-auto mt-4">
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+                  {data?.hackathon.map(
+                    (hackathon: Hackathon) =>
+                      hackathon.name.includes(search) && (
+                        <HackathonCard
+                          key={hackathon.id}
+                          name={hackathon.name}
+                          description={hackathon.description || ""}
+                          url={hackathon.url}
+                        />
+                      ),
+                  )}
+                </div>
               </div>
+            </>
+          ) : (
+            <div className="flex flex-col items-center justify-center">
+              <Up>
+                <h1 className="mb-2 text-2xl font-medium">Welcome</h1>
+              </Up>
+              <Down delay={0.2}>
+                <div className="flex flex-col items-center justify-center">
+                  <p className="mb-2 text-center text-neutral-300">
+                    You don&apos;t have any hackathons yet. Create one now!
+                  </p>
+                  <ArrowDown width={32} className="mb-2" />
+                  <CreateNew />
+                </div>
+              </Down>
             </div>
-          </>
-        ) : (
-          <div className="flex flex-col items-center justify-center">
-            <Up>
-              <h1 className="mb-2 text-2xl font-medium">Welcome</h1>
-            </Up>
-            <Down delay={0.2}>
-              <div className="flex flex-col items-center justify-center">
-                <p className="mb-2 text-center text-neutral-300">
-                  You don&apos;t have any hackathons yet. Create one now!
-                </p>
-                <ArrowDown width={32} className="mb-2" />
-                <CreateNew />
+          )}
+        </div>
+        <h1 className="mb-4 mt-5 text-2xl font-medium">Participations</h1>
+        <div className="container mx-auto mt-4">
+          {data?.participants && data?.participants?.length > 0 ? (
+            <>
+              <div className="container mx-auto mt-4">
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+                  {data?.participants.map((participant) => (
+                    <>
+                      <p>{participant.title}</p>
+                    </>
+                  ))}
+                </div>
               </div>
-            </Down>
-          </div>
-        )}
+            </>
+          ) : (
+            <div className="flex flex-col items-center justify-center">
+              <Up>
+                <h1 className="mb-2 text-2xl font-medium">Or participate</h1>
+              </Up>
+              <Down delay={0.2}>
+                <div className="flex flex-col items-center justify-center">
+                  <p className="mb-2 text-center text-neutral-300">
+                    Enter a hackathon key
+                  </p>
+                  <ArrowDown width={32} className="mb-2" />
+                  <EnterKey />
+                </div>
+              </Down>
+            </div>
+          )}
+        </div>
         {error && (
           <Tip>
             <p className="text-red-500">
