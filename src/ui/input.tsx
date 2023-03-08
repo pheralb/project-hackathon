e878fbbb6e1615ebdce63dc9e002908f0e8a84a3
pheralb <pablohdez4340@gmail.com>
2023-03-08 00:46:40 +0000

    ✨ Input improvements.

--- src/ui/input.tsx
+++ src/ui/input.tsx
@@ -6,7 +6,7 @@ export interface InputProps
 
 // For react-hook-forms inputs :'
 export const inputStyles =
-  "focus:ring-primary-500 mt-2 mb-2 block w-full rounded-lg border border-neutral-800 bg-midnight py-2 px-3 text-white placeholder-neutral-400 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-opacity-75";
+  "focus:ring-primary-500 mt-2 mb-2 block w-full rounded-lg border border-neutral-800 bg-midnight py-2 px-3 text-white placeholder-neutral-400 focus:border-transparent focus:outline outline-neutral-600 outline-1";
 
 const Input = (props: InputProps) => {
   return <input className={inputStyles} {...props} />;
