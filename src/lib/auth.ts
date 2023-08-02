8719dc04be1a7a805ec79fd63b8f197a67a577f4
pheralb <pablohdez4340@gmail.com>
2023-08-02 12:29:19 +0100

    🛠️ Separate authOptions.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,70 @@
 import { type GetServerSidePropsContext } from "next";
-import { getServerSession } from "next-auth";
+import { NextAuthOptions, getServerSession } from "next-auth";
 
-import { authOptions } from "../pages/api/auth/[...nextauth]";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+import GithubProvider from "next-auth/providers/github";
+import { env } from "@/env/index.mjs";
+
+export const authOptions: NextAuthOptions = {
+  // Callbacks:
+  callbacks: {
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.username = token.username;
+        session.user.email = token.email;
+        session.user.image = token.image;
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      const dbUser = await prisma.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      });
+      if (!dbUser) {
+        token.id = user?.id;
+        return token;
+      }
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        username: dbUser.username,
+        email: dbUser.email,
+        image: dbUser.image,
+      };
+    },
+  },
+  // Providers:
+  providers: [
+    GithubProvider({
+      clientId: env.GITHUB_CLIENT_ID || "",
+      clientSecret: env.GITHUB_CLIENT_SECRET || "",
+      profile(profile) {
+        return {
+          id: profile.id.toString(),
+          name: profile.name || profile.login,
+          username: profile.login,
+          email: profile.email,
+          image: profile.avatar_url,
+        };
+      },
+    }),
+  ],
+  // Pages:
+  pages: {
+    signIn: "/auth",
+  },
+  // Session:
+  session: {
+    strategy: "jwt",
+  },
+  // Database:
+  adapter: PrismaAdapter(prisma),
+};
 
 export const getServerAuthSession = async (ctx: {
   req: GetServerSidePropsContext["req"];
